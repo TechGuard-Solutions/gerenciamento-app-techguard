370430c68bd7b7773a9ac7d4c0e5ec5701fc3c4d
Gustavo Gil <gustavo.gil@sptech.school>
2024-10-15 17:31:31 -0300

    url log sistema

--- logSistema.sh
+++ logSistema.sh
@@ -29,7 +29,7 @@ echo "======================" >> "$LOG_FILE"
 echo "Log concluído." >> "$LOG_FILE"
 
 echo "Enviando log para o S3: $S3_BUCKET"
-aws s3 cp "$LOG_FILE" s3://$S3_BUCKET/logs/logSistema/
+aws s3 cp "$LOG_FILE" s3://$S3_BUCKET/logs/logsSistema/
 
 if [ $? -eq 0 ]; then
   echo "Log enviado com sucesso para o S3."
