589b9bde5b52d2df1ea6add8a47b1af7f3dd85a4
Gustavo Gil <gustavo.gil@sptech.school>
2024-10-27 04:19:16 -0300

    alteração crontab

--- instalacaoCompletaComDocker.sh
+++ instalacaoCompletaComDocker.sh
@@ -180,16 +180,10 @@ echo -e "${YELLOW}Criando Dockerfile com imagem JAVA...${NC}"
 DOCKERFILE="Dockerfile"
 cat <<EOF >$DOCKERFILE
 FROM openjdk:21
-
 RUN apt install -y cron && \
-echo "0 16 * * * java -jar target/Integracao-1.0-SNAPSHOT-jar-with-dependencies.jar" > /etc/cron.d/mycron
-crontab /etc/cron.d/mycron
-
 WORKDIR /usr/src/app
 COPY conexao-java/ /usr/src/app/
-
 COPY start.sh /usr/src/app/start.sh
-
 EXPOSE 3030
 CMD ["/usr/src/app/start.sh"]
 EOF
